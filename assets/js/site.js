8436f83087c4cad79ad231543b14c78cdd552aea
meyamisa-rgb <meyamisa02@gmail.com>
2026-03-29 23:45:09 +0200

    Restore Amygdala process galleries

--- assets/js/site.js
+++ assets/js/site.js
@@ -215,10 +215,10 @@
 
     renderInto(
       "#amygdala-meyamisa-grid",
-      Array.from({ length: 19 }, (_, index) =>
+      [2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19].map((imageNumber) =>
         createImageCard({
-          src: `../assets/images/amygdala/meyamisa-${String(index + 1).padStart(2, "0")}.jpeg`,
-          alt: `Veritas exponere image ${index + 1}`,
+          src: `../assets/images/amygdala/meyamisa-${String(imageNumber).padStart(2, "0")}.jpeg`,
+          alt: `Veritas exponere image ${imageNumber}`,
           gallery: "amygdala-meyamisa",
         })
       ).join("")
